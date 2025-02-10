42bbeba5f0bda380403796bd4da7f814f1c16c09
sabina <shaikhsabina637@gmail.com>
2025-02-10 19:40:19 +0530

    changes the port no

--- backend/server.js
+++ backend/server.js
@@ -7,12 +7,12 @@ const PORT =  process.env.PORT || 4000;
 // for json data to accept
 app.use(express.json());
 app.use(cors({ origin: [
-    "http:://localhost:5175",
+    "http://localhost:5175",
     "http://localhost:5173"
 ], 
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials:true,
-    allowedHeaders:["Content-Type","Authorization"]
+    allowedHeaders:["Content-Type","Authorization","withCredentials"]
 }))
 require("dotenv").config();
 // import the routes
