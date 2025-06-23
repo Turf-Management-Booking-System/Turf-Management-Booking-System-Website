aa21979a813af9de22fa51ed47fb30b7daa581ed
sabina <shaikhsabina637@gmail.com>
2025-06-23 20:42:45 +0530

    necessary changes done

--- Frontend/src/components/pages/Dashboard.jsx
+++ Frontend/src/components/pages/Dashboard.jsx
@@ -1,4 +1,3 @@
-"use client"
 
 import { useState, useContext, useEffect } from "react"
 import { motion, AnimatePresence } from "framer-motion"
--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
     base: '/', // This ensures assets load correctly on Render
+    server: {
+    historyApiFallback: true, // For dev server
+  },
 })
